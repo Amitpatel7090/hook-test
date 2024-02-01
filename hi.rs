628fb51456a0377514dc575290d670cce0b1d638
Amitpatel7090 <amit7090114@gmail.com>
2024-02-01 08:54:03 +0530

    rs file added

--- hi.rs
+++ hi.rs
@@ -2,7 +2,7 @@ fn main(){
     println!("Hello, this is the first commitsd!");
     println!(" commiting failed!");
         println!(" post commit added!");
-                println!(" tessting pre -push!");
+                println!(" tessting post -push!");
 
 
 }
