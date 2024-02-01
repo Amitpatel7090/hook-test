15dc9acba02dedebaab963491a750d46095b12aa
Amitpatel7090 <amit7090114@gmail.com>
2024-02-01 08:56:56 +0530

    Revert "rs file added"
    This reverts commit 113292545c0b1a01e067a3c3533f7c80405d6cbf.

--- hi.rs
+++ hi.rs
@@ -2,7 +2,7 @@ fn main(){
     println!("Hello, this is the first commitsd!");
     println!(" commiting failed!");
         println!(" post commit added!");
-                println!(" tessting post -push!");
+                println!(" tessting pre -push!");
 
 
 }
