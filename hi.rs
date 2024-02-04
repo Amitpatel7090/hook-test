8f39017f0322a53e6b759cb23ac2d8c9c7b4901a
AMIT PATEL <130010976+Amitpatel7090@users.noreply.github.com>
2024-02-04 19:22:24 +0530

    precommit test (#3)
    * this is my first commit
    
    * commit without capital
    
    * commit with lowercase
    
    * yaml file added
    
    * commit without capital
    
    * yaml changed
    
    * try
    
    * updated
    
    * modified
    
    * updated
    
    * UPDATED
    
    * post commit added
    
    * test
    
    * commitwith vs code
    
    * set pre push
    
    * c file added
    
    * testing
    
    * pre-push testings
    
    * testing reverting commit
    
    * Revert "testing reverting commit"
    
    This reverts commit c20bbea3451872e706b43c44eb385aa19c421c57.
    
    * reverted successfully
    
    * post push applied
    
    * patch msg hook added
    
    * Revert "patch msg hook added"
    
    This reverts commit 537d4ec24c9aaca4f047647e771861fb8339dabb.
    
    * patch hook aplied
    
    * Revert "patch hook aplied"
    
    This reverts commit d0b48e383627f6b206bdfc1b93953068917dc282.
    
    * patch hook applied
    
    * Revert "patch hook applied"
    
    This reverts commit 037acd6f8c983505574ce15c3e33b608238feee6.
    
    * hook added
    
    * Revert "hook added"
    
    This reverts commit 7c6dfea3cdf83c1c12c759e7d93dc6bfa9f03ae4.
    
    * hi hook added
    
    * Revert "hi hook added"
    
    This reverts commit bb0695679119d6c2cb2cc0960d32d1f15134b8b7.
    
    * c file added
    
    * Revert "c file added"
    
    This reverts commit 70c74a865fec21e6d405c64d208d78fd5e7aa331.
    
    * c file  added
    
    * Revert "c file  added"
    
    This reverts commit 337371648ff245bc4d35a37711888ba923b4efe0.
    
    * rs file added
    
    * Revert " rs file added"
    
    This reverts commit 628fb51456a0377514dc575290d670cce0b1d638.
    
    * rs file added
    
    * Revert "rs file added"
    
    This reverts commit 113292545c0b1a01e067a3c3533f7c80405d6cbf.
    
    * c added
    
    * new c added
    
    * Revert "new c added"
    
    This reverts commit 28596ce8996676affeabec37b3449d871d255744.
    
    * c file added

---
+++ hi.rs
@@ -0,0 +1,8 @@
+fn main(){
+    println!("Hello, this is the first commitsd!");
+    println!(" commiting failed!");
+        println!(" post commit added!");
+                println!(" tessting pre -push!");
+
+
+}
